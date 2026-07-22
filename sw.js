b9824a77fb5fc4820f929360a47b1e65ea608f4c
Osnayro <osnay.r@gmail.com>
2026-07-22 08:39:12 -0400

    Refactor cache handling in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = 'conti-conti-v1';
 const ASSETS = [
+  './',
   './index.html',
   './manifest.json',
   './logo-colegio.jpg',
@@ -10,12 +11,16 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
